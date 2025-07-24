69cc7cf3970be0ea9a71582958c341f68abbcd1c
Artur Małas <artur.malas25@gmail.com>
2025-07-24 09:49:24 +0200

    chore<backend>: basic setup with placeholder

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import axios from "axios";
+import pg from "pg";
+import { config } from "./config.js"
+
+const app = express();
+const port = 3000;
+const db = new pg.Client(config.dbConfig);
+db.connect()
+    .then(() => console.log(`Connected to DB ${config.dbConfig.database}`))
+    .catch(err => console.log('DB Connection Error: ', err));
+
+app.use(express.static("public"));
+
+// Example for now
+let books = [
+    {id: 1, title: "The Lord of the Rings", author: "J.R.R. Tolkien"},
+    {id: 2, title: "Star Wars Thrawn", author: "Timothy Zahn"}
+];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render(
+        'index.ejs',
+        { books: books }
+    );
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
