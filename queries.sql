4561e3aec14bb89f8f43c7d8d327bfec8f7cef69
Artur Małas <artur.malas25@gmail.com>
2025-07-24 10:37:34 +0200

    chore<sql>: create queries to insert example books into the table

--- queries.sql
+++ queries.sql
@@ -5,3 +5,8 @@ CREATE TABLE books (
     author VARCHAR(200) NOT NULL,
     UNIQUE(title, author)
 );
+
+-- Insert example books into the table
+INSERT INTO books (title, author) VALUES
+    ('The Lord of the Rings', 'J.R.R. Tolkien'),
+    ('Star Wars Thrawn', 'Timothy Zahn');
